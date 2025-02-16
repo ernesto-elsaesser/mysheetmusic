5f3ce21c4aba845efd3cb8b3726492306197fffd
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-02-16 16:29:58 +0000

    Relative key refactoring

--- render.js
+++ render.js
@@ -1,12 +1,22 @@
+const PITCHES = "xcdefgabcdefga".split("")
+
+const CHORDS = {
+    i: -1, ii: -2, iii: -3, iv: -4, v: -5, vi: -6,
+    I: 1, II: 2, III: 3, IV: 4, V: 5, VI: 6,
+}
+
+// TODO other signatures with sharps and flats
+const KEYS = {
+    C: 0, Am: +5
+}
+
 function renderSong(textarea, sheet) {
 
     let bars = textarea.value.split("\n\n")
-
-    let factory = new Vex.Flow.Factory({
-        renderer: { elementId: sheet.id, width: 0, height: 0 },
-    })
-
-    let easy = factory.EasyScore()
+    let header = bars.shift().split(" ")
+    let keyShift = KEYS[header[0]]
+    let scale = PITCHES.slice(keyShift)
+    let octave = parseInt(header[1])
 
     sheet.innerHTML = ""
 
@@ -14,22 +24,100 @@ function renderSong(textarea, sheet) {
     for (let bar of bars) {
         if (bar == "") continue
         let lines = bar.split("\n")
-        let harmony = lines.shift()
-        let melody = lines.shift()
-        if (melody == "") continue
-        let chords = harmony.trim().split(/ +/)
-        let notes = easy.notes(melody)
-        if (lines.length < maxLines) lines.push("&nbsp;")
-        else maxLines = lines.length
-        let element = createBar(chords, notes, lines)
+        if (lines[0] == "") continue
+        while (lines.length < maxLines) lines.push("&nbsp;")
+        if (lines.length > maxLines) maxLines = lines.length
+
+        let element = document.createElement("div")
+        element.className = "bar"
+
+        try {
+            createBar(element, lines, scale, octave)
+        } catch (error) {
+            element.innerText = "INVALID DATA"
+            element.style.color = "red"
+        }
+
+
         sheet.appendChild(element)
     }
 }
 
-function createBar(chords, notes, lines) {
+function createBar(element, lines, scale, octave) {
 
-  let element = document.createElement("div")
-  element.className = "bar"
+  let parts = lines.shift().split(": ")
+  let chord = parts[0]
+  let melody = parts[1].split(" ")
+
+  var notes = []
+  var extras = []
+  for (var i = 0; i < melody.length; i += 1) {
+
+    let parts = melody[i].split("/")
+    var pitchCode = parts[0]
+    var duration = parts[1]
+    let flags = parts.length > 2 ? parts[2] : ""
+
+    var pitch = "b"
+    var oct = octave
+    if (pitchCode == "0") {
+        duration += "r"
+    } else {
+        while (pitchCode[0] == "-") {
+            oct -= 1
+            pitchCode = pitchCode.slice(1)
+        }
+        while (pitchCode[0] == "+") {
+            oct += 1
+            pitchCode = pitchCode.slice(1)
+        }
+        pitch = scale[parseInt(pitchCode)]
+    }
+
+    let key = pitch + "/" + oct.toString()
+    let note = new Vex.Flow.StaveNote({ keys: [key], duration: duration })
+    notes.push(note)
+
+    if (flags.includes("~")) {
+        let tie = new Vex.Flow.StaveTie({
+            first_note: notes[i - 1],
+            last_note: note,
+            first_indices: [0],
+            last_indices: [0],
+        })
+        extras.push(tie)
+    }
+
+    if (flags.includes("$")) {
+        let tie = new Vex.Flow.StaveTie({
+            first_note: note,
+            first_indices: [0],
+        })
+        extras.push(tie)
+    }
+
+    if (flags.includes("3")) {
+        let triplet = new Vex.Flow.Tuplet(notes.slice(i-2), {location: -1})
+        extras.push(triplet)
+    }
+
+  }
+
+  if (chord != "") {
+    let code = CHORDS[chord]
+    var text = ""
+    if (code < 0) {
+        text = scale[-code].toUpperCase() + "m"
+    } else {
+        text = scale[code].toUpperCase()
+    }
+
+    let symbol = new Vex.Flow.ChordSymbol()
+    symbol.setHorizontal('center')
+    symbol.setFontSize(14)
+    symbol.addText(text)
+    notes[0].addModifier(symbol)
+  }
 
   let width = 60 + notes.length * 32
   let renderer = new Vex.Flow.Renderer(element, Vex.Flow.Renderer.Backends.SVG)
@@ -40,56 +128,9 @@ function createBar(chords, notes, lines) {
   element.style.width = scaledWidth.toString() + "px"
 
   let stave = new Vex.Flow.Stave(0, 0, width)
-
-  var extras = []
-  for (var i = 0; i < notes.length; i += 1) {
-      let chord = chords[i]
-      if ((chord == undefined) || (chord == ".")) continue
-
-      if (chord[0] == "~") {
-          let tie = new Vex.Flow.StaveTie({
-              first_note: notes[i-1],
-              last_note: notes[i],
-              first_indices: [0],
-              last_indices: [0],
-          })
-          extras.push(tie)
-          if (chord.length == 1) continue
-          chord = chord.slice(1)
-      }
-
-      if (chord[0] == "3") {
-          let triplet = new Vex.Flow.Tuplet(notes.slice(i, i+3), {location: -1})
-          extras.push(triplet)
-          if (chord.length == 1) continue
-          chord = chord.slice(1)
-      }
-
-      let symbol = new Vex.Flow.ChordSymbol()
-      symbol.setHorizontal('center')
-      symbol.setFontSize(14)
-      symbol.addText(chord)
-      notes[i].addModifier(symbol)
-  }
-
-  if (chords[notes.length] == "~") {
-    let tie = new Vex.Flow.StaveTie({
-        first_note: notes[notes.length-1],
-        first_indices: [0],
-    })
-    extras.push(tie)
-  }
-
   stave.setContext(context).draw()
-
-  try {
-    Vex.Flow.Formatter.FormatAndDraw(context, stave, notes, true)
-  } catch (error) {
-    let msg = document.createElement("div")
-    msg.innerText = "INVALID DATA"
-    msg.style.color = "red"
-    element.appendChild(msg)
-  }
+  
+  Vex.Flow.Formatter.FormatAndDraw(context, stave, notes, true)
   
   extras.forEach(e => e.setContext(context).draw())
 
@@ -99,6 +140,4 @@ function createBar(chords, notes, lines) {
       lyrics.innerHTML = lines[i]
       element.appendChild(lyrics)
   }
-
-  return element
 }

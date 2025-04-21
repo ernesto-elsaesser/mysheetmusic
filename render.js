fe60b68fd80f1aa81220c9c53c861eb160b9dcb8
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-04-21 15:12:45 +0000

    Redesign editing

--- render.js
+++ render.js
@@ -5,188 +5,140 @@ const PITCHES = {
     "1''": "c/6", "2''": "d/6", "3''": "e/6", "4''": "f/6",  "5''": "g/6", 
 }
 
-function renderCode(code, verse, sheet, color) {
+function renderMeasure(frame, isDark, width, melody, tieEnd) {
 
-    let measures = code.split("\n\n")
+    frame.innerHTML = ""
 
-    sheet.innerHTML = ""
+    let notes = []
+    let ties = []
+    let triplets = []
+    let inTriplet = false
 
-    var rowCount = 1
-    for (let i = 0; i < measures.length; i += 1) {
-        let measure = measures[i]
-        if (measure == "") continue
-        let lines = measure.split("\n")
-        if (lines[0] == "") continue
-
-        let melody = lines[0]
-        let text = lines[verse] ?? lines[1] ?? ""
-        if (lines.length > rowCount) rowCount = lines.length
-
-        let nextMeasure = measures[i+1]
-        let tieEnd = nextMeasure && (nextMeasure[0] == "~")
-
-        let element = document.createElement("div")
-        element.className = "measure"
-
-        try {
-            createMeasure(element, color, melody, text, tieEnd)
-        } catch (error) {
-            console.log(error)
-            element.innerText = error.toString()
-            element.style.color = "red"
-        }
-
-        sheet.appendChild(element)
+    for (let code of melody.split(" ")) {
+  
+      const data = code.split("")
+  
+      let tie = false
+      if (data[0] == "~") {
+          tie = true
+          data.shift()
+      }
+  
+      let degree = data.shift()
+      while ((data[0] == "'") || (data[0] == ",")) {
+          degree += data.shift()
+      }
+      let pitch = PITCHES[degree]
+  
+      let mods = []
+  
+      if (data[0] == "#") {
+          const sharp = new Vex.Flow.Accidental("#")
+          mods.push(sharp)
+          data.shift()
+      }
+  
+      if (data[0] == "b") {
+          let flat = new Vex.Flow.Accidental("b")
+          mods.push(flat)
+          data.shift()
+      }
+  
+      var duration = data.shift()
+      duration = duration.replace("o", "8")
+      duration = duration.replace("x", "16")
+      duration = duration.replace("z", "32")
+  
+      let dots = 0
+      if (data[0] == ".") {
+          let dot = new Vex.Flow.Dot()
+          mods.push(dot)
+          dots = 1
+          data.shift()
+      }
+  
+      if (degree == "0") {
+          pitch = "b/4"
+          duration += "r"
+      }
+  
+      let note = new Vex.Flow.StaveNote({
+          clef: "treble",
+          keys: [pitch],
+          duration: duration,
+          dots: dots,
+          auto_stem: true,
+      })
+      notes.push(note)
+  
+      if (data[0] == "t") {
+          data.shift()
+          if (inTriplet && triplets[0].length < 3) {
+              triplets[0].push(note)
+          } else {
+              triplets.unshift([note])
+              inTriplet = true
+          }
+      } else {
+          inTriplet = false
+      }
+  
+      mods.forEach(m => note.addModifier(m))
+  
+      if (tie) {
+          ties.push({
+              first_note: notes[notes.length - 2],
+              last_note: note,
+              first_indices: [0],
+              last_indices: [0],
+          })
+      }
+  
+      if (data.length == 0) continue
+  
+      const chordDegree = data.shift()
+      let chord = PITCHES[chordDegree].slice(0, 1).toUpperCase()
+      chord += data.join("")
+  
+      let symbol = new Vex.Flow.ChordSymbol()
+      symbol.setHorizontal('center')
+      symbol.setFontSize(14)
+      symbol.addText(chord)
+      note.addModifier(symbol)
     }
-
-    return rowCount
-}
-
-function createMeasure(element, color, melody, text, tieEnd) {
-
-  let codes = melody.split(" ")
-
-  let notes = []
-  let ties = []
-  let triplets = []
-  let inTriplet = false
-  let noteWidth = 0
-  for (let i = 0; i < codes.length; i += 1) {
-
-    const data = codes[i].split("")
-
-    let tie = false
-    if (data[0] == "~") {
-        tie = true
-        data.shift()
+  
+    if (tieEnd) {
+      ties.push({
+          first_note: notes[notes.length - 1],
+          first_indices: [0],
+      })
     }
-
-    let degree = data.shift()
-    while ((data[0] == "'") || (data[0] == ",")) {
-        degree += data.shift()
-    }
-    let pitch = PITCHES[degree]
-
-    let mods = []
-
-    if (data[0] == "#") {
-        const sharp = new Vex.Flow.Accidental("#")
-        mods.push(sharp)
-        noteWidth += 1
-        data.shift()
-    }
-
-    if (data[0] == "b") {
-        let flat = new Vex.Flow.Accidental("b")
-        mods.push(flat)
-        noteWidth += 1
-        data.shift()
-    }
-
-    var duration = data.shift()
-    duration = duration.replace("o", "8")
-    duration = duration.replace("x", "16")
-    duration = duration.replace("z", "32")
-
-    let dots = 0
-    if (data[0] == ".") {
-        let dot = new Vex.Flow.Dot()
-        mods.push(dot)
-        noteWidth += 0.5
-        dots = 1
-        data.shift()
-    }
-
-    if (degree == "0") {
-        pitch = "b/4"
-        duration += "r"
-    }
-
-    let note = new Vex.Flow.StaveNote({
-        clef: "treble",
-        keys: [pitch],
-        duration: duration,
-        dots: dots,
-        auto_stem: true,
+  
+    const scaledWidth = width * 1.25
+    let renderer = new Vex.Flow.Renderer(frame, Vex.Flow.Renderer.Backends.SVG)
+    renderer.resize(scaledWidth, 100)
+    let context = renderer.getContext()
+    context.scale(0.8, 0.8)
+    
+    const color = isDark ? "white" : "black"
+    context.setFillStyle(color)
+    context.setStrokeStyle(color)
+  
+    let stave = new Vex.Flow.Stave(0, 0, scaledWidth)
+    stave.setDefaultLedgerLineStyle({ strokeStyle: "#999", lineWidth: 1.4 })
+    stave.setContext(context).draw()
+  
+    Vex.Flow.Formatter.FormatAndDraw(context, stave, notes, true)
+  
+    ties.forEach(options => {
+        const tie = new Vex.Flow.StaveTie(options)
+        tie.setContext(context).draw()
     })
-    notes.push(note)
-    noteWidth += 1
-
-    if (data[0] == "t") {
-        data.shift()
-        if (inTriplet && triplets[0].length < 3) {
-            triplets[0].push(note)
-        } else {
-            triplets.unshift([note])
-            inTriplet = true
-        }
-    } else {
-        inTriplet = false
-    }
-
-    mods.forEach(m => note.addModifier(m))
-
-    if (tie) {
-        ties.push({
-            first_note: notes[i - 1],
-            last_note: note,
-            first_indices: [0],
-            last_indices: [0],
-        })
-    }
-
-    if (data.length == 0) continue
-
-    const chordDegree = data.shift()
-    const chord = PITCHES[chordDegree].slice(0, 1).toUpperCase() + data.join("")
-
-    let symbol = new Vex.Flow.ChordSymbol()
-    symbol.setHorizontal('center')
-    symbol.setFontSize(14)
-    symbol.addText(chord)
-    note.addModifier(symbol)
-  }
-
-  if (tieEnd) {
-    ties.push({
-        first_note: notes[notes.length - 1],
-        first_indices: [0],
+  
+    triplets.forEach(notes => {
+        const tuplet = new Vex.Flow.Tuplet(notes, {location: Vex.Flow.Tuplet.LOCATION_BOTTOM})
+        tuplet.setContext(context).draw()
     })
-  }
-
-  let width = 50 + (noteWidth * 20)
-  const textWidth = text.length * 9
-  if (textWidth > width) width = textWidth
-
-  element.style.width = (width * 0.8).toString() + "px"
-
-  let renderer = new Vex.Flow.Renderer(element, Vex.Flow.Renderer.Backends.SVG)
-  renderer.resize(width, 100)
-  let context = renderer.getContext()
-  context.scale(0.8, 0.8)
-  context.setFillStyle(color)
-  context.setStrokeStyle(color)
-
-  let stave = new Vex.Flow.Stave(0, 0, width)
-  stave.setDefaultLedgerLineStyle({ strokeStyle: "#999", lineWidth: 1.4 })
-  stave.setContext(context).draw()
-
-  Vex.Flow.Formatter.FormatAndDraw(context, stave, notes, true)
-
-  ties.forEach(options => {
-    let tie = new Vex.Flow.StaveTie(options)
-    tie.setContext(context).draw()
-  })
-
-  triplets.forEach(notes => {
-    let tuplet = new Vex.Flow.Tuplet(notes, {location: Vex.Flow.Tuplet.LOCATION_BOTTOM})
-    tuplet.setContext(context).draw()
-  })
-
-  let lyrics = document.createElement("div")
-  lyrics.className = "lyrics"
-  lyrics.innerHTML = text
-  element.appendChild(lyrics)
 }
 
 function transposeCode(code, steps) {

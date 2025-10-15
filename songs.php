0f15c3af8f6dec38647494684239a5d630a4fd74
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-10-15 06:57:27 +0000

    Support dots in song names

--- songs.php
+++ songs.php
@@ -25,9 +25,11 @@ if (isset($_POST['name'])) {
 $files = scandir($DIR);
 $songs = array();
 foreach( $files as $file ) {
-    $parts = explode('.', $file);
-    if ($parts[1] != 'txt') continue;
-    $name = $parts[0];
+    $dotpos = strrpos($file, '.');
+    $ext = substr($file, $dotpos + 1);
+    $name = substr($file, 0, $dotpos);
+    if ($ext != 'txt') continue;
+    $name = substr($file, 0, -4);
     $songs[$name] = file_get_contents($DIR . '/' . $file);
 }
 

60bb254ef787124d48dc00b7ec5717389a485ee1
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-10-22 17:18:05 +0000

    Adjust song list

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         if ($ext != 'txt') continue;
         $name = substr($file, 0, -4);
         $song = file_get_contents($DIR . '/' . $file);
-        print("<a href=\"song.php?name=$name&verse=1\">$name</a><br/>");
+        print("<a href=\"song.php?name=$name\">$name</a><br/>");
     }
 ?>
     </div>

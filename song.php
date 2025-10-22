f684754c4e739b708f1e0baf9956be15fe513e38
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-10-22 20:44:26 +0000

    Implement snapshots

--- song.php
+++ song.php
@@ -2,20 +2,20 @@
 <html lang="en">
 
 <?php
-    $method = $_SERVER['REQUEST_METHOD'];
-    $name = $_GET['name'];
-    $file = 'songs/' . $name . '.txt';
+$method = $_SERVER['REQUEST_METHOD'];
+$name = $_GET['name'];
+$text_file = 'songs/' . $name . '.txt';
 
 if ($method == 'POST') {
-    file_put_contents($file, $_POST['code']);
-    $success = chmod($file, 0666);
+    file_put_contents($text_file, $_POST['code']);
+    $success = chmod($text_file, 0666);
     if (!$success) {
         $error = error_get_last();
         echo $error['message'];
         exit;
     }
 } else if ($method == 'DELETE') {
-    $success = unlink($file);
+    $success = unlink($text_file);
     if ($success) {
         echo "Deleted.";
     } else {
@@ -28,7 +28,7 @@ if ($method == 'POST') {
 $verse = 1;
 if (isset($_GET['verse'])) $verse = intval($_GET['verse']);
 
-$song = file_get_contents($file);
+$song = file_get_contents($text_file);
 $parts = explode("\n\n", $song);
 $n = count($parts);
 ?>
@@ -46,27 +46,37 @@ $n = count($parts);
     <div id="content">
         <div id="sheet">
 <?php
-    $verse_count = 1;
-    for($i = 0; $i < $n; $i += 1) {
-        $part = $parts[$i];
-        if ($part == "") continue;
+$verse_count = 1;
+$use_snapshot = $_GET['snap'];
+for($i = 0; $i < $n; $i += 1) {
+    $part = $parts[$i];
+    if ($part == "") continue;
 
-        $lines = explode("\n", $part);
-        $melody = $lines[0];
-        $verse_count = max($verse_count, count($lines) - 1);
-        $text = $lines[$verse];
-        $maxlen = max(strlen($melody), strlen($text));
-        $width = max(($maxlen + 1) * 8, 40);
+    $lines = explode("\n", $part);
+    $melody = $lines[0];
+    $verse_count = max($verse_count, count($lines) - 1);
 
-        $tieEnd = 0;
-        if ($i + 1 < $n && $parts[$i + 1][0] == "~") $tieEnd = 1;
+    if ($use_snapshot) continue;
 
-        echo "<div class=\"measure\" style=\"width: ${width}px\">";
-        echo "<div id=\"p$i\" class=\"frame\"></div>";
-        echo "<div class=\"lyrics\">$text</div>";
-        echo "<script>vx($i, $width, \"$melody\", $tieEnd);</script>";
-        echo "</div>";
-    }
+    $text = $lines[$verse];
+    $maxlen = max(strlen($melody), strlen($text));
+    $width = max(($maxlen + 1) * 8, 40);
+
+    $tieEnd = 0;
+    if ($i + 1 < $n && $parts[$i + 1][0] == "~") $tieEnd = 1;
+
+    echo "<div class=\"measure\" style=\"width: ${width}px\">";
+    echo "<div id=\"p$i\" class=\"frame\"></div>";
+    echo "<div class=\"lyrics\">$text</div>";
+    echo "<script>vx($i, $width, \"$melody\", $tieEnd);</script>";
+    echo "</div>";
+}
+
+if ($use_snapshot) {
+    $html_file = 'rendered/' . $name . '.html';
+    $html = file_get_contents($html_file);
+    echo $html;
+}
 ?>
         </div>
     </div>
@@ -80,7 +90,19 @@ $n = count($parts);
             echo "<a href=\"song.php?name=$name&verse=$v\">VERSE $v</a>";
         }
     }
+        echo "&nbsp;&nbsp;|&nbsp;&nbsp;";
+        echo "<a onclick=\"snapshot()\">SNAP</a>";
 ?>
     </div>
+    <script>
+        function snapshot() {
+            const sheet = document.getElementById("sheet")
+            const html = sheet.innerHTML
+            fetch('<?php echo "render.php?name=$name"; ?>', {
+                method: 'POST',
+                body: html
+            }).then((res) => window.alert(res.statusText))
+        }
+    </script>
 </body>
 </html>

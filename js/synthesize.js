a935bffa5da07d09329510d83b74d2011330265c
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2026-01-07 17:50:55 +0000

    Add synthesis

---
+++ js/synthesize.js
@@ -0,0 +1,50 @@
+const A4_DELTAS = [null, -9/12, -7/12, -5/12, -4/12, -2/12, 0, 2/12]
+const DURATION_FACTORS = {
+    "w": 4.0,
+    "h": 2.0,
+    "q": 1.0,
+    "o": 0.5,
+    "x": 0.25,
+    "z": 0.125,
+}
+const TEMPO = 0.5
+
+function synthesizeMelody(song) {
+
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+    let startTime = audioCtx.currentTime
+
+    song.forEach((part) => {
+        part.notes.forEach((note) => {
+
+            let factor = DURATION_FACTORS[note.duration]
+            if (note.dots == 1) factor *= 1.5
+            else if (note.dots == 2) factor *= 1.75
+
+            const endTime = startTime + TEMPO * factor
+
+            if (note.degree > 0) {
+                let exp = A4_DELTAS[note.degree]
+                exp += note.octave - 4
+                if (note.accs == "#") exp += 1/12
+                else if (note.accs == "b") exp -= 1/12
+                const frequency = 440 * Math.pow(2, exp)
+                const osc = audioCtx.createOscillator()
+                osc.frequency.setValueAtTime(frequency, startTime)
+                
+                // envelope
+                const gain = audioCtx.createGain()
+                gain.gain.setValueAtTime(0, startTime)
+                gain.gain.linearRampToValueAtTime(0.1, startTime + 0.02)
+                gain.gain.exponentialRampToValueAtTime(0.0001, endTime)
+
+                osc.connect(gain)
+                gain.connect(audioCtx.destination)
+
+                osc.start(startTime)
+                osc.stop(endTime)
+            }
+            startTime = endTime
+        })
+    })
+}
--- song.php
+++ song.php
@@ -49,6 +49,7 @@ if (file_exists($snap)) $sheet = file_get_contents($snap);
     <script src="https://cdn.jsdelivr.net/npm/vexflow@4.2.5/build/cjs/vexflow.js"></script>
     <script src="js/parse.js?v=1"></script>
     <script src="js/render.js?v=2"></script>
+    <script src="js/synthesize.js?v=1"></script>
     <script src="js/transpose.js?v=1"></script>
     <script src="js/import.js?v=1"></script>
     <link rel="stylesheet" href="style.css?v=1" />
@@ -72,6 +73,7 @@ if (file_exists($snap)) $sheet = file_get_contents($snap);
         </div>
     </div>
     <div id="footer">
+        <input type="button" onclick="playSong()" value="PLAY" />
         <input type="button" onclick="editSong()" value="EDIT" />
         <input type="button" onclick="scrollText(-1)" value="<" />
         <input type="button" onclick="scrollText(1)" value=">" />
@@ -150,6 +152,12 @@ if (file_exists($snap)) $sheet = file_get_contents($snap);
             sheet.hidden = false
             editor.hidden = true
             footer.hidden = false
+        }
+
+        function playSong() {
+
+            const song = parseCode(code.value)
+            synthesizeMelody(song)
         }
 
         async function saveSong() {
